74b3d8ac9c8fbd5015c3218ced274f9e279c9e95
bmartin86 <iMuy11pMpk>
2022-03-11 16:06:46 +0100

    ispravak css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,4 @@
 module.exports = {
-  content: [
-    module.exports = {
-      content: [
-        "./resources/layouts/app.blade.php",
-        "./resources/js/app.js",
-        "./resources/**/*.vue",
-      ],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    }
-  ],
   theme: {
     extend: {},
   },
