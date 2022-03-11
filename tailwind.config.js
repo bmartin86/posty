a66801d2127d909b46db168cb9eb3fea52f0fe3e
bmartin86 <iMuy11pMpk>
2022-03-11 15:24:25 +0100

    dodan tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    module.exports = {
+      content: [
+        "./resources/layouts/app.blade.php",
+        "./resources/js/app.js",
+        "./resources/**/*.vue",
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
