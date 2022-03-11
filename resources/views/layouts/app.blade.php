395f89487d3d5dbb7daf6687cc7155595309f88a
bmartin86 <iMuy11pMpk>
2022-03-11 22:32:08 +0100

    izmjena layouta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,18 +22,22 @@
             </li>
         </ul>
         <ul class="flex items-center">
-            <li>
-                <a href="" class="p-3">Martin</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="{{ route('register') }}" class="p-3">Register</a>
-            </li>
-            <li>
+            @auth
+                <li>
+                    <a href="" class="p-3">Martin</a>
+                </li>
+                <li>
                 <a href="" class="p-3">Logout</a>
             </li>
+            @endauth
+            @guest
+                <li>
+                    <a href="" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
         </ul>
 
     </nav>
